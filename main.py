2d85b07515f1f38044e1958c20fff30741c5ddec
Lucas Cipriano <lucashcipriano14@gmail.com>
2023-09-18 21:41:19 -0300

    Adicionando branch Develop

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import re
 
-    livros - {ID0{}}
-    usuarios = { ID0 : {"Nome": nome, "Email": email, "Senha": senha, "Idade": idade}}
+def listar_livros(livros: list)-> None:
+    """
+    Faz um print com todos os livros em ordem do último lançado para o mais antigo
+    :return: None
+    """
+    listagem = ""
+    for i in livros:
+        listagem = listagem + f"""
+            Nome: {i["nome"]}
+            Autor: {i["autor"]}
+            lançamento: {i["lancamento"]}
+            gênero: {i["genero"]}
+            avaliações: {i["avaliacoes"]}
+            sinopse: {i["sinopse"]}"""
+
