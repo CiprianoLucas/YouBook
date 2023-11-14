35e343a1043fd2a0788c757e129fdef2d6f69b83
angekasten <angeekasten@gmail.com>
2023-11-13 22:38:31 -0300

    uploadpage4real

--- static/js/bookpage.js
+++ static/js/bookpage.js
@@ -1,71 +1,16 @@
 document.getElementById("enviarLivros").addEventListener("click", cadastrarLivros);
 
-function saveBooks(books) {
-    localStorage.setItem('books', JSON.stringify(books));
-}
-
-function getBooks() {
-    const storedBooks = localStorage.getItem('books');
-    return storedBooks ? JSON.parse(storedBooks) : [];
-}
-
-function renderBooks() {
-    const livrosContainer = document.getElementById("livrosContainer");
-    const books = getBooks();
-
-    livrosContainer.innerHTML = ''; // Clear existing content
-
-    books.forEach(book => {
-        const cardDiv = document.createElement('div');
-        cardDiv.classList.add('col-sm-3');
-
-        const card = document.createElement('div');
-        card.classList.add('card', 'border-dark');
-
-        const img = document.createElement('img');
-        img.src = book.coverImage; // Assuming you have coverImage property in your book object
-        img.classList.add('card-img-top');
-
-        const cardBody = document.createElement('div');
-        cardBody.classList.add('card-body');
-
-        const buttonsDiv = document.createElement('div');
-        buttonsDiv.classList.add('d-grid', 'gap-2', 'd-md-block');
-
-        const lerButton = document.createElement('a');
-        lerButton.href = book.pdfUrl;
-        lerButton.classList.add('btn', 'btn-primary');
-        lerButton.type = 'button';
-        lerButton.textContent = 'Ler o livro';
-
-        const downloadButton = document.createElement('a');
-        downloadButton.href = book.pdfUrl;
-        downloadButton.download = '';
-        downloadButton.classList.add('btn', 'btn-danger');
-        downloadButton.type = 'button';
-        downloadButton.textContent = 'Baixar o livro';
-
-        buttonsDiv.appendChild(lerButton);
-        buttonsDiv.appendChild(downloadButton);
-
-        cardBody.appendChild(buttonsDiv);
-        card.appendChild(img);
-        card.appendChild(cardBody);
-        cardDiv.appendChild(card);
-
-        livrosContainer.appendChild(cardDiv);
-    });
-}
-
 function cadastrarLivros() {
     const nomeLivro = document.getElementById("nomeLivro").value;
     const nomeAutor = document.getElementById("nomeAutor").value;
     const genero = document.getElementById("genero").value;
     const nomeUsuario = document.getElementById("nomeUsuario").value;
     const pdfFiles = document.getElementById("pdfInput").files;
+    const imagemCapaFile = document.getElementById("imagemCapaInput").files[0]; // Apenas um arquivo de imagem é permitido
+    const livrosContainer = document.querySelector('.livros-container');
+    const visualizador = document.querySelector('.visualizador');
+    const cadastro = document.querySelector('.cadastro');
 
-    const books = getBooks();
-    
     for (let i = 0; i < pdfFiles.length; i++) {
         const pdfFile = pdfFiles[i];
 
@@ -74,20 +19,80 @@ function cadastrarLivros() {
             return;
         }
 
-        books.push({
-            title: nomeLivro,
-            author: nomeAutor,
-            genre: genero,
-            pdfUrl: URL.createObjectURL(pdfFile),
-            // Add other properties as needed
+        const livroDiv = document.createElement('div');
+        livroDiv.classList.add('livro');
+
+        // Adiciona detalhes do livro
+        const detalhes = document.createElement('div');
+        detalhes.classList.add('detalhes');
+        detalhes.innerHTML = `
+            <h2>${nomeLivro}</h2>
+            <p>Autor: ${nomeAutor}</p>
+            <p>Gênero: ${genero}</p>
+        `;
+        livroDiv.appendChild(detalhes);
+
+        // Adiciona imagem de capa
+        if (imagemCapaFile) {
+            const imagemCapa = document.createElement('img');
+            imagemCapa.src = URL.createObjectURL(imagemCapaFile);
+            imagemCapa.alt = 'Imagem de Capa';
+            imagemCapa.style.maxWidth = '100%';
+            livroDiv.appendChild(imagemCapa);
+        }
+
+        // Adiciona botão para abrir em nova aba
+        const abrirNovaAbaBtn = document.createElement('button');
+        abrirNovaAbaBtn.textContent = 'Abrir em Nova Aba';
+        abrirNovaAbaBtn.addEventListener('click', function () {
+            window.open(URL.createObjectURL(pdfFile), '_blank');
         });
+        livroDiv.appendChild(abrirNovaAbaBtn);
+
+        // Adiciona botão de download
+        const downloadBtn = document.createElement('button');
+        downloadBtn.textContent = 'Download';
+        downloadBtn.addEventListener('click', function () {
+            const a = document.createElement('a');
+            a.href = URL.createObjectURL(pdfFile);
+            a.download = `${nomeLivro}.pdf`;
+            a.style.display = 'none';
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        });
+        livroDiv.appendChild(downloadBtn);
+
+        // Adiciona botão para enviar comentário
+        const enviarComentarioBtn = document.createElement('button');
+        enviarComentarioBtn.textContent = 'Enviar Comentário';
+        enviarComentarioBtn.addEventListener('click', function () {
+            const comentario = prompt('Digite seu comentário:');
+            if (comentario) {
+                const novoComentario = document.createElement('p');
+                novoComentario.textContent = `Comentário (${nomeUsuario}): ${comentario}`;
+                livroDiv.appendChild(novoComentario);
+            }
+        });
+        livroDiv.appendChild(enviarComentarioBtn);
+
+        livrosContainer.appendChild(livroDiv);
     }
 
-    saveBooks(books);
+    cadastro.style.display = 'none';
+    visualizador.style.display = 'block';
 
-    // Redirect the user to the catalog page
-    window.location.href = "catalog.html";
+    const adicionarLivro = document.getElementById("adicionarLivro");
+    if (livrosContainer.childNodes.length > 0) {
+        adicionarLivro.style.display = 'block';
+    }
+    adicionarLivro.addEventListener("click", function () {
+        cadastro.style.display = 'block';
+        visualizador.style.display = 'none';
+    });
+
+    document.getElementById("voltarCadastro").addEventListener("click", function () {
+        cadastro.style.display = 'block';
+        visualizador.style.display = 'none';
+    });
 }
-
-// Initial rendering when the page loads
-renderBooks();
