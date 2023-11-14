faa901e74e3bcfd3796a7740dd10361e1d07254f
Pedro Sena <pedroprogrampy@gmail.com>
2023-11-13 22:05:20 -0300

    feat: :art: adição do jquerry

--- static/js/pageuserpub.js
+++ static/js/pageuserpub.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    $(".sobremim").hide();
-
     const hidebook = () => {
         $(".livros").hide();
         $(".sobremim").show();
@@ -9,9 +7,49 @@ $(document).ready(function () {
         $(".sobremim").hide();
         $(".livros").show();
     };
+    const hideuser = () => {
+        $("#userInfo").hide();
+        $("#userEdit").hide();
+        $("#userInput").show();
+    };
+    
+    const showdesc = () => {
+        $("#descricao").hide();
+        $("#newAbout").show();
+        $("#saveDesc").show();
+
+    };
+
+    const newuser = () => {
+        const newName = $("#editName").val();
+        const newBio = $("#editBio").val();
+
+        $("#userInfo p:first-child strong").text(newName);
+        $("#userInfo p:last-child").text(newBio);
+
+        $("#userInput").hide();
+        $("#userInfo").show();
+        $("#userEdit").show();
+    };
+
+    const newdesc = () => {
+        const $descricaoNova = $("#newAbout");
+        $("#descricao").text($descricaoNova.val());
+
+        $("#descricao").show();
+        $("#newAbout").hide();
+        $("#saveDesc").hide();
+
+    };
+
 
     $("#book").on("click", hideabout);
     $("#about").on("click", hidebook);
+    $("#userEdit").on("click", hideuser);
+    $("#saveEdit").on("click", newuser);
+    $("#showEdit").on("click", showdesc);
+    $("#saveDesc").on("click", newdesc);
+
 });
 
 
