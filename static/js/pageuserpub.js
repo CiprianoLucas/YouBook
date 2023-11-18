06bfbe9fcf598f9537cb885b9acb0e06265b78fb
Pedro Sena <pedroprogrampy@gmail.com>
2023-11-17 22:54:27 -0300

    fix: :art: adicionando links e consertando css

--- static/js/pageuserpub.js
+++ static/js/pageuserpub.js
@@ -12,9 +12,18 @@ $(document).ready(function () {
         $("#userEdit").hide();
         $("#userInput").show();
     };
-    
+
     const showdesc = () => {
         $("#descricao").hide();
+        $("#showEdit").hide();
+        $("#social1").hide();
+        $("#social2").hide();
+        $("#social3").hide();
+        $("#social4").hide();
+        $("#social1Edit").show();
+        $("#social2Edit").show();
+        $("#social3Edit").show();
+        $("#social4Edit").show();
         $("#newAbout").show();
         $("#saveDesc").show();
 
@@ -24,8 +33,14 @@ $(document).ready(function () {
         const newName = $("#editName").val();
         const newBio = $("#editBio").val();
 
-        $("#userInfo p:first-child strong").text(newName);
-        $("#userInfo p:last-child").text(newBio);
+        if (newName.trim() !== "") {
+            $("#userInfo p:first-child strong").text(newName);
+            $("#userInfo p:last-child").text(newBio);
+        }
+
+        $("#editName").val("");
+        $("#editBio").val("");
+
 
         $("#userInput").hide();
         $("#userInfo").show();
@@ -34,12 +49,46 @@ $(document).ready(function () {
 
     const newdesc = () => {
         const $descricaoNova = $("#newAbout");
-        $("#descricao").text($descricaoNova.val());
+        const $twitter = $("#social4Edit");
+        const $instagram = $("#social1Edit");
+        const $github = $("#social3Edit");
+        const $linkedin = $("#social2Edit");
 
+        if ($descricaoNova.val().trim() !== "") {
+            $("#descricao").text($descricaoNova.val());
+        }
+        if ($twitter.val().trim() !== "") {
+            $("#social4").attr("href", $twitter.val());
+        }
+        if ($instagram.val().trim() !== "") {
+            $("#social1").attr("href", $instagram.val());
+        }
+        if ($linkedin.val().trim() !== "") {
+            $("#social2").attr("href", $linkedin.val());
+        }
+        if ($github.val().trim() !== "") {
+            $("#social3").attr("href", $github.val());
+        }
+
+        $descricaoNova.val("");
+        $twitter.val("");
+        $instagram.val("");
+        $linkedin.val("");
+        $github.val("");
+
+        $("#social1").show();
+        $("#social2").show();
+        $("#social3").show();
+        $("#social4").show();
+        $("#showEdit").show();
         $("#descricao").show();
         $("#newAbout").hide();
         $("#saveDesc").hide();
-
+        $("#social1Edit").hide();
+        $("#social2Edit").hide();
+        $("#social3Edit").hide();
+        $("#social4Edit").hide();
+        
     };
 
 
@@ -49,7 +98,6 @@ $(document).ready(function () {
     $("#saveEdit").on("click", newuser);
     $("#showEdit").on("click", showdesc);
     $("#saveDesc").on("click", newdesc);
-
 });
 
 
